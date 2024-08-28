cf3d1091dcd1cabe147f8618c558a5795acb88cc
FarhanaBeevi2024 <FarhanaBeevi2015@gmail.com>
2024-08-28 09:00:11 +0530

    initial version

---
+++ model/userModel.js
@@ -0,0 +1,27 @@
+//import mongoose
+const mongoose = require('mongoose')
+
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isAdmin:{
+        type: Boolean,
+        required: false,
+        default: false
+    }
+})
+const users = mongoose.model("users", userSchema)
+
+module.exports = users
---
+++ routes.js
@@ -0,0 +1,19 @@
+//1) import express
+const express = require('express')
+
+//import userController
+const userController=require('./controller/userController')
+
+//2) create an object for router class
+const router = new express.Router()
+
+//3) set up path for each request from view
+
+//register
+router.post('/register',userController.registerController)
+
+//login
+router.post('/login', userController.loginController)
+
+//4) export the router
+module.exports = router
